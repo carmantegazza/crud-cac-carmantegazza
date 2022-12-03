ec379017a42c333335bc56f90530a8fd0ea807c9
Charly Cimino <carlosecimino@gmail.com>
2022-12-03 14:13:44 -0300

    Para empezar

---
+++ src/main/java/ar/charlycimino/cac/crud/controlador/AppServlet.java
@@ -0,0 +1,19 @@
+
+package ar.charlycimino.cac.crud.controlador;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+@WebServlet(name = "AppServlet", urlPatterns = {"/app"})
+public class AppServlet extends HttpServlet {
+
+    /*
+        PENDIENTE...
+    */
+}
